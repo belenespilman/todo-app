3a2b1533933d2577edf55126457712f7dc9df20f
belenespilman <belen.espilman@gmail.com>
2024-02-21 19:55:49 +0100

    styling syncronization

--- src/ChangeAlert/index.js
+++ src/ChangeAlert/index.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import { withStorageListener } from './withStorageListener'
+import './ChangeAlert.css'
 
 function ChangeAlert({ show, toggleShow }) {
   if (show) {
     return (
       <React.Fragment>
-        <div>
-          <p>hubo cambios</p>
-          <button onClick={() => toggleShow(false)}>Reload</button>
+        <div className="update-container">
+          <p className="update-alert">Your TODO list is outdated</p>
+          <button className="reload-button" onClick={() => toggleShow(false)}>
+            Refresh
+          </button>
         </div>
       </React.Fragment>
     )
